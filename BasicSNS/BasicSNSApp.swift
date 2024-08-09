93fcff38ba0692b4cf013bf677bad0d2c07c0d5f
Christan Shane Plaza <christanshaneplaza@gmail.com>
2024-08-09 20:31:52 +0800

    Initial Commit

---
+++ BasicSNS/BasicSNSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicSNSApp.swift
+//  BasicSNS
+//
+//  Created by mbp on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BasicSNSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
